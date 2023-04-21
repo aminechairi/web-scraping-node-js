e35522456fe9fcc44e664f1febae613fdffd1b9e
amine <aminechairi2000@gmail.com>
2023-04-21 04:20:15 +0300

    edite

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const fs = require('fs');
 const workbook = new ExcelJS.Workbook();
 const worksheet = workbook.addWorksheet('Data');
 
-let url = 'https://glovoapp.com/ma/fr/tetouan/mcdonaldsr-tet55/?content=sauces-c.1415115344';
+let url = "https://glovoapp.com/ma/fr/tetouan/tele-pizza/?content=menu-c.174232486";
+
+let fileName = "TEST";
 
 axios.get(url)
   .then(response => {
@@ -15,63 +17,89 @@ axios.get(url)
 
     // Extract data from the website
     const data = [];
-
-    $("div[data-test-id='list-element']").map((i, item) => {
+    
+    $("[data-test-id='grid-elements']").map((i, item) => {
       
-      let names = $(item)
-        .children("[data-test-id='product-row-content']")
-        .children(".product-row__content")
-        .children(".product-row__info")
-        .children(".product-row__name")
+      let name = $(item)
+        .children("div")
+        .children(".tile__description")
         .children("span")
-        .children('span')
         .text();
 
-      let imgs = $(item)
-        .children("[data-test-id='product-row-content']")
-        .children(".product-row__content")
+      let price = $(item)
+        .children("div")
+        .children(".tile__price")
+        .children(".product-price")
+        .children("div")
+        .children("span")
+        .text();
+
+      let image = $(item)
+        .children("div")
+        .children(".tile__background")
         .children("img")
         .attr("src");
 
-      let prices = $(item)
-        .children("[data-test-id='product-row-content']")
-        .children(".product-row__bottom")
-        .children(".product-price")
-        .children("span")
-        .text();
-
         data.push(
           {
-            names: names.trim(),
-            imgs: imgs.trim(),
-            prices: prices.trim(),
+            name: `${name}`.trim(),
+            price: `${price}`.trim(),
+            image: `${image}`.trim(),
           },
         );
 
     });
 
+    // $("[data-test-id='list-element']").map((i, item) => {
+      
+    //   let name = $(item)
+    //     .children("div")
+    //     .children(".product-row__content")
+    //     .children(".product-row__info")
+    //     .children(".product-row__name")
+    //     .children("span")
+    //     .text();
+
+    //   let price = $(item)
+    //     .children("div")
+    //     .children(".product-row__bottom")
+    //     .children(".product-price")
+    //     .children("div")
+    //     .children("span")
+    //     .text();
+
+    //   let image = $(item)
+    //     .children("div")
+    //     .children(".product-row__content")
+    //     .children("img")
+    //     .attr("src");
+
+    //     data.push(
+    //       {
+    //         name: `${name}`.trim(),
+    //         price: `${price}`.trim(),
+    //         image: `${image}`.trim(),
+    //       },
+    //     );
+
+    // });
+
     console.log(
       data
     );
 
-    // // Save data to a JSON file
-    // fs.writeFile('./dataJson/SAUCES.json', JSON.stringify(data), err => {
-    //   if (err) throw err;
-    //   console.log('Data saved to data.json');
-    // });
+    // Add data to the worksheet
+    worksheet.columns = [
+      { header: 'Names', key: 'name' },
+      { header: 'Prices', key: 'price' },
+      { header: 'images', key: 'image' },
+    ];
+    data.forEach(item => {
+      worksheet.addRow(item);
+    });
 
-    // // Add data to the worksheet
-    // worksheet.columns = [
-    //   { header: 'Names', key: 'names' },
-    //   { header: 'Prices', key: 'prices' },
-    //   { header: 'Imgs', key: 'imgs' },
-    // ];
-    // data.forEach(item => {
-    //   worksheet.addRow(item);
-    // });
-
-    // // Save the workbook to a file
-    // return workbook.xlsx.writeFile('./dataExcel/SAUCES.xlsx');
+    // Save the workbook to a file
+    return workbook.xlsx.writeFile(`./dataExcel/${fileName}.xlsx`);
 
   })
   .then(() => {
